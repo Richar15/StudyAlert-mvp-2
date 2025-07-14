3ec27256860773c0b05b6e9a36ecd8dd47bfed47
BeginerLord <messismark0@gmail.com>
2025-07-14 16:53:35 -0500

    Refactor code structure and remove redundant changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:studyalert_mvp_2/login/login.dart';
 
 void main() {
+  // Asegurarse de que el splash screen se muestre correctamente
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  
+  // Configurar orientación preferida
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  
   runApp(const MyApp());
 }
 
@@ -11,11 +23,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Remover el splash screen una vez que la app está lista
+    FlutterNativeSplash.remove();
+    
     return MaterialApp(
       title: 'StudyAlert',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: const LoginScreen(),
     );
